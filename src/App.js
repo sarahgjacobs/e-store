df1b827fc91429209838da64f21a4670c3de1aa0
Sarah Jacobs <sgjacobs14@gmail.com>
2023-08-14 09:35:48 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to E-Store</h1>
+        <h3>Save 30% on every purchase for the next 5 days. Act fast before it's gone!</h3>
+        
       </header>
     </div>
   );
